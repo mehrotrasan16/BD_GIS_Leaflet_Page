35d0cb26a17da60ca4cab96b7620505e978fcb76
Sanket <sanketmehrotra102@gmail.com>
2020-06-30 03:53:58 -0600

    created more_map to redesign a simpler design with focus on only the first five user inputs: geospatial, temporal, dataset, feature, and filter. added preset temporal filters working on adding filters according to feature and preset spatial filters also have to work on combining shape inputs into layers and creating multiple filters.

---
+++ Datasetfeatures.js
@@ -0,0 +1,13 @@
+var censusfeatures = {
+    "Sex Ratio":["Male","Female","Ratio"],
+    "Mean Income":["Below Poverty line",">500$/month"],
+    "Population Density":[">50",">80",">100"],
+    "Race":["Hispanic","Asian","American","American Indian"]
+};
+var waterfeatures = {
+    "Resevoirs":["horsetooth","timnath",],
+    "Lakes" : ["Warren"],
+    "Dams":["Hoover"],
+    "Flood Prone areas": ["5 year","10 year"]
+};
+var infrafeatures = [   "hospitals",    "farmers markets"]
