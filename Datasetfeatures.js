f24d6b03e32f5be3a9a16ef8d489bc2b25550ead
Sanket <sanketmehrotra102@gmail.com>
2020-09-06 19:14:04 -0600

    Updating repo with last work done. Dev on hold for now.

--- Datasetfeatures.js
+++ Datasetfeatures.js
@@ -1,13 +1,13 @@
 var censusfeatures = {
     "Sex Ratio":["Male","Female","Ratio"],
-    "Mean Income":["Below Poverty line",">500$/month"],
+    "Mean Income":["<100$/month",">500$/month"],
     "Population Density":[">50",">80",">100"],
     "Race":["Hispanic","Asian","American","American Indian"]
 };
 var waterfeatures = {
-    "Resevoirs":["horsetooth","timnath",],
+    "Resevoirs":["Horsetooth","Timnath"],
     "Lakes" : ["Warren"],
     "Dams":["Hoover"],
     "Flood Prone areas": ["5 year","10 year"]
 };
-var infrafeatures = [   "hospitals",    "farmers markets"]
+var infrafeatures = [   "Hospitals",    "Food Distribution Areas"]
